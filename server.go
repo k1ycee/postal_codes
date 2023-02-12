345e6d9d084889063258c3dba5b6b571373a1664
k1ycee <chiagozieani145@gmail.com>
2023-02-12 23:29:23 +0100

    Benue and Kogi state

--- server.go
+++ server.go
@@ -1,12 +1,23 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
+	jsonContent, err := ioutil.ReadFile("./postal_codes.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString(string(jsonContent))
 	})
 
 	app.Listen(":3000")
